4a21f2aca0ee0ce00c499ad1cd6a3082712244c9
Andreas Bresser <andreas.bresser@dfki.de>
2024-10-14 09:41:39 +0200

    Bugfix: export build libraries again and update to 0.0.4

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // Reexport your entry components here
+import GamepadManager from "./components/GamepadManager.svelte";
+import VirtualJoystick from "./components/VirtualJoystick.svelte";
+import { gamepad } from "./store/gamepad.js";
+import { gamepad_listener } from "./store/gamepad_listener.js";
+import { distance, angle, findCoord, radians, degrees, clamp } from './utils.js';
+export { GamepadManager, VirtualJoystick, gamepad, gamepad_listener, distance, angle, findCoord, radians, degrees, clamp };
