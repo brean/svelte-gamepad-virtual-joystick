1a87790b28519cfc7109542d16fa6e5c7b956488
Andreas Bresser <self@andreasbresser.de>
2025-02-04 21:26:15 +0100

    update version and prepare to build

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,20 @@
 // Reexport your entry components here
 import GamepadManager from "./components/GamepadManager.svelte";
+import KeyboardManager from "./components/KeyboardManager.svelte";
 import VirtualJoystick from "./components/VirtualJoystick.svelte";
-import { gamepad } from "./store/gamepad.js";
-import { gamepad_listener } from "./store/gamepad_listener.js";
+import VirtualButton from "./components/VirtualButton.svelte";
+import VirtualList from "./components/VirtualList.svelte";
+
+import type VirtualJoystickInput from "./models/VirtualJoystickInput.js";
+import type VirtualButtonInput from "./models/VirtualButtonInput.js";
+import type VirtualListInput from "./models/VirtualListInput.js";
+
+import '$lib/store/gamepad_callbacks.svelte.js'
+import '$lib/store/keyboard_callbacks.svelte.js'
+import '$lib/store/virtual_input.svelte.js'
+
 import { distance, angle, findCoord, radians, degrees, clamp } from './utils.js';
 export { 
-    GamepadManager, VirtualJoystick, gamepad, gamepad_listener,
+    GamepadManager, KeyboardManager, VirtualButton, VirtualJoystick, VirtualList, 
     distance, angle, findCoord, radians, degrees, clamp };
+export type {VirtualButtonInput, VirtualJoystickInput, VirtualListInput}
